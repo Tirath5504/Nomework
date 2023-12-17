edd8f09a79afcad82afeafd957393dc40ddb4648
vaxad <varadprabhu111@gmail.com>
2023-12-17 10:47:44 +0530

    frontend+backend

---
+++ backend/index.js
@@ -0,0 +1,17 @@
+import express from "express"
+import cors from "cors"
+import textPrompt from "./routes/textprompt.js"
+const app = express()
+const port = 3000
+app.use(express.json())
+app.use(cors())
+
+app.use("/textprompt", textPrompt);
+app.get("/", (req, res) => {
+    res.send("workin")
+})
+
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
